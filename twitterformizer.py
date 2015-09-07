f3f2f485f64e5bcd43d353c325e1c3e251280b93
Toby Oliver <toby@typeform.com>
2015-09-07 13:57:21 +0200

    move module magic to typeformizer module

--- twitterformizer.py
+++ twitterformizer.py
@@ -6,7 +6,7 @@ import urlparse
 import logging
 import BeautifulSoup
 from CREDS import *
-typeformizer = __import__('typeformizer')
+import typeformizer
 logging.basicConfig(level=logging.WARN)
 
 find_url = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
@@ -30,25 +30,7 @@ def main():
 	    
 	    if match:
 		found_url = match.group(0)
-		try:
-		    r = requests.get(found_url)
-		    actual_url = r.url
-		    logging.warn('got url %s' % actual_url)
-		except Exception as e:
-		    print "couldn't get url - %s" % found_url
-		    print e
-
-		# now see if we can convert this this url
-		for link_re in typeformizer.forms_links:
-		    match = re.search(link_re['re'],actual_url)
-		    if match:
-			logging.warn("converting form ... %s %s" % (actual_url,link_re['class']))
-
-			converter = getattr(typeformizer,link_re['class'])(r.text)
-			typeform = converter.to_typeform()
-			typeform_url = typeform.submit()
-			logging.warn("generated typeform ... %s " % typeform_url)
-			break
+                typeformizer.convert(found_url)
 
     except TwitterSearchException as e: # take care of all those ugly errors if there are some
         print(e)
--- typeformizer/__init__.py
+++ typeformizer/__init__.py
@@ -1,4 +1,7 @@
 import re
+import sys
+import requests
+import logging
 from convert_monkey import monkey_to_typeform
 from convert_google_forms import google_forms_to_typeform
 
@@ -8,3 +11,29 @@ forms_links = [
         {'class':'monkey_to_typeform', 're':monkey_url},
         {'class':'google_forms_to_typeform', 're':google_forms_url}
 ]
+
+def convert(url):
+    try:
+        r = requests.get(url)
+        actual_url = r.url
+        logging.warn('got url %s' % actual_url)
+    except Exception as e:
+        print "couldn't get url - %s" % url
+        print e
+        return None
+
+    # now see if we can convert this this url
+    typeform_url = None
+    for link_re in forms_links:
+        match = re.search(link_re['re'],actual_url)
+        if match:
+            logging.warn("converting form ... %s %s" % (actual_url,link_re['class']))
+
+            converter = getattr(sys.modules[__name__],link_re['class'])(r.text)
+            typeform = converter.to_typeform()
+            typeform_url = typeform.submit()
+            logging.warn("generated typeform ... %s " % typeform_url)
+            break
+
+    return typeform_url
+
