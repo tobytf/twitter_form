1880e2b6c9b88adc61c70299c48d960d0d1bd748
tobytf <toby@typeform.com>
2015-09-26 09:29:03 +0200

    trying to create a form - from scratch not working

---
+++ ruby_config.rb
@@ -0,0 +1,9 @@
+Configuration.for('urls'){
+      base_url 'https://admin.typeform.com/'
+}
+
+Configuration.for('user'){
+      email 'demo@typeform.com'
+      correct_pw 'password'
+      incorrect_pw 'passwordx'
+}
--- test_ruby.rb
+++ test_ruby.rb
@@ -11,3 +11,11 @@ PageObject.default_element_wait = $waits['pageobject_default_element_wait']
 # enter form
 go_to_sign_up_page_url
 on_page(WelcomePage).skip_welcome_page
+@form = 'example'
+#check_db_user_does_not_have_form(@form, @email)
+on_page(WorkspacePage).create_form_from_scratch(@form)
+
+fields = ['Short Text', 'Email']
+fields.each do |field|
+  on_page(BuildPage).add_field_with_default_text(field)
+end
